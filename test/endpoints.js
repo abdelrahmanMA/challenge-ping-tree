65f2f5b97ae54e4b0cb334f4be332b3e385eedb1
Abdelrahman Muhammad <bme.abdelrahmanmuhammed@gmail.com>
2022-09-24 07:09:38 +0200

    test: add more tests for validation and error handling

--- test/endpoints.js
+++ test/endpoints.js
@@ -17,7 +17,7 @@ test.serial.cb('healthcheck', function (t) {
   })
 })
 
-test.serial.cb('Adding New Target', function (t) {
+test.serial.cb('adding new target', function (t) {
   redis.FLUSHDB()
   var url = '/api/targets'
   var opts = { method: 'POST', encoding: 'json' }
@@ -39,7 +39,84 @@ test.serial.cb('Adding New Target', function (t) {
   }
 })
 
-test.serial.cb('Get All Target', function (t) {
+test.serial.cb('should fail adding new target when duplicate',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/targets'
+    var opts = { method: 'POST', encoding: 'json' }
+    var newTarget = _getDummyTarget()
+
+    _seedRedis([newTarget])
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(newTarget))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Target exists', 'status is ok')
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail adding new target when missing fields',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/targets'
+    var opts = { method: 'POST', encoding: 'json' }
+    var newTarget = _getDummyTarget()
+
+    delete newTarget.value
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(newTarget))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Required fields are missing', 'status is ok')
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail adding new target when field is of the wrong type',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/targets'
+    var opts = { method: 'POST', encoding: 'json' }
+    var newTarget = _getDummyTarget()
+
+    newTarget.accept.geoState = 'ca'
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(newTarget))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'hour and geoState must be array', 'status is ok')
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail adding new target when sending empty data',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/targets'
+    var opts = { method: 'POST', encoding: 'json' }
+
+    servertest(server(), url, opts, onResponse).end('{}')
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Required fields are missing', 'status is ok')
+      t.end()
+    }
+  })
+
+test.serial.cb('get all target', function (t) {
   redis.FLUSHDB()
   var url = '/api/targets'
   var opts = { method: 'GET', encoding: 'json' }
@@ -62,7 +139,7 @@ test.serial.cb('Get All Target', function (t) {
   }
 })
 
-test.serial.cb('Get Target By Id', function (t) {
+test.serial.cb('get target by id', function (t) {
   redis.FLUSHDB()
   var url = '/api/target/1'
   var opts = { method: 'GET', encoding: 'json' }
@@ -83,7 +160,46 @@ test.serial.cb('Get Target By Id', function (t) {
   }
 })
 
-test.serial.cb('Update Target By Id', function (t) {
+test.serial.cb('should fail getting target by id when id is not a number',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/target/id'
+    var opts = { method: 'GET', encoding: 'json' }
+    var dummyTarget = _getDummyTarget()
+
+    _seedRedis([dummyTarget])
+
+    servertest(server(), url, opts, onResponse)
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Id param is required', 'status is ok')
+
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail getting target by id when target does not exist',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/target/1'
+    var opts = { method: 'GET', encoding: 'json' }
+
+    servertest(server(), url, opts, onResponse)
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 404, 'correct statusCode')
+      t.is(res.body.status, 'Target does not exist', 'status is ok')
+
+      t.end()
+    }
+  })
+
+test.serial.cb('update target by id', function (t) {
   redis.FLUSHDB()
   var url = '/api/target/1'
   var opts = { method: 'POST', encoding: 'json' }
@@ -111,7 +227,78 @@ test.serial.cb('Update Target By Id', function (t) {
   }
 })
 
-test.serial.cb('Target Decision', function (t) {
+test.serial.cb('should fail updating target by id when sending empty data',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/target/1'
+    var opts = { method: 'POST', encoding: 'json' }
+    var dummyTarget = _getDummyTarget()
+
+    _seedRedis([dummyTarget])
+
+    // Update target properties
+    dummyTarget.value = '1'
+    dummyTarget.maxAcceptsPerDay = '50'
+
+    servertest(server(), url, opts, onResponse).end('{}')
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'At least one field is required', 'status is ok')
+
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail updating target by id when target does not exist',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/target/1232'
+    var opts = { method: 'POST', encoding: 'json' }
+    var dummyTarget = _getDummyTarget()
+
+    _seedRedis([dummyTarget])
+
+    // Update target properties
+    dummyTarget.value = '1'
+    dummyTarget.maxAcceptsPerDay = '50'
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(dummyTarget))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 404, 'correct statusCode')
+      t.is(res.body.status, 'Target does not exist', 'status is ok')
+
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail updating target by id when id is not a number',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/api/target/id'
+    var opts = { method: 'POST', encoding: 'json' }
+    var dummyTarget = _getDummyTarget()
+
+    _seedRedis([dummyTarget])
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(dummyTarget))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Id param is required', 'status is ok')
+
+      t.end()
+    }
+  })
+
+test.serial.cb('target decision', function (t) {
   redis.FLUSHDB()
   var url = '/route'
   var opts = { method: 'POST', encoding: 'json' }
@@ -130,6 +317,137 @@ test.serial.cb('Target Decision', function (t) {
     t.end()
   }
 })
+
+test.serial.cb('should reject target when different state', function (t) {
+  redis.FLUSHDB()
+  var url = '/route'
+  var opts = { method: 'POST', encoding: 'json' }
+  var visitor = _getDummyVisitor({ geoState: 'kk' })
+  var dummyTarget = _getDummyTarget()
+
+  _seedRedis([dummyTarget])
+
+  servertest(server(), url, opts, onResponse).end(JSON.stringify(visitor))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.decision, 'reject', 'found target')
+    t.end()
+  }
+})
+
+test.serial.cb('should reject target when different timestamp', function (t) {
+  redis.FLUSHDB()
+  var url = '/route'
+  var opts = { method: 'POST', encoding: 'json' }
+  var visitor = _getDummyVisitor({ timestamp: '2018-07-19T23:28:59.513Z' })
+  var dummyTarget = _getDummyTarget()
+
+  _seedRedis([dummyTarget])
+
+  servertest(server(), url, opts, onResponse).end(JSON.stringify(visitor))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.decision, 'reject', 'found target')
+    t.end()
+  }
+})
+
+test.serial.cb('should find highest target', function (t) {
+  redis.FLUSHDB()
+  var url = '/route'
+  var opts = { method: 'POST', encoding: 'json' }
+  var visitor = _getDummyVisitor()
+  var dummyTargets = [
+    _getDummyTarget(),
+    _getDummyTarget({ id: 2, value: '60', url: 'example2.com' })
+  ]
+
+  _seedRedis(dummyTargets)
+
+  servertest(server(), url, opts, onResponse).end(JSON.stringify(visitor))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.decision, dummyTargets[1].url, 'found target')
+    t.end()
+  }
+})
+
+test.serial.cb('should find lower target when the highest has reached accepts',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/route'
+    var opts = { method: 'POST', encoding: 'json' }
+    var visitor = _getDummyVisitor()
+    var dummyTargets = [
+      _getDummyTarget(),
+      _getDummyTarget({ id: 2, value: '60', url: 'example2.com' })
+    ]
+
+    _seedRedis(dummyTargets)
+    redis.set('target:2:acceptsToday', 10)
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(visitor))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.is(res.body.decision, dummyTargets[0].url, 'found target')
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail target decision when missing visitor fields',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/route'
+    var opts = { method: 'POST', encoding: 'json' }
+    var visitor = _getDummyVisitor()
+    var dummyTarget = _getDummyTarget()
+
+    _seedRedis([dummyTarget])
+
+    delete visitor.timestamp
+
+    servertest(server(), url, opts, onResponse).end(JSON.stringify(visitor))
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Required field are missing', 'status is ok')
+      t.end()
+    }
+  })
+
+test.serial.cb('should fail target decision when sending empty visitor',
+  function (t) {
+    redis.FLUSHDB()
+    var url = '/route'
+    var opts = { method: 'POST', encoding: 'json' }
+    var dummyTarget = _getDummyTarget()
+
+    _seedRedis([dummyTarget])
+
+    servertest(server(), url, opts, onResponse).end('{}')
+
+    function onResponse (err, res) {
+      t.falsy(err, 'no error')
+
+      t.is(res.statusCode, 400, 'correct statusCode')
+      t.is(res.body.status, 'Required field are missing', 'status is ok')
+      t.end()
+    }
+  })
 
 function _getDummyTarget (overrides) {
   return {
